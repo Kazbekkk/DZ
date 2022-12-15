b7fa53c8188f23d94e76a598ed67e8a637b87d57
Kazbek <bender.rodrigiz2014@gmail.com>
2022-12-15 20:36:10 +0300

    Нахожение максимального из 3 чисел

---
+++ Max_Min_3Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int d = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (a < d)
+{
+    if (b < d)
+    {
+        Console.WriteLine($"Максимальное число {d}");
+    }
+    else
+    {
+        Console.WriteLine($"Максимальное число {b}");
+    }
+}
+else
+{
+    if (b < a)
+    {
+        Console.WriteLine($"Максимальное число {a}");
+    }
+    else
+    {
+        Console.WriteLine($"Максимальное число {b}");
+    }
+}

b9d10ace2fcc2d69565f02b222a68177c31da296
Kazbek <bender.rodrigiz2014@gmail.com>
2022-12-15 19:46:26 +0300

    Нахождение мин и мах

---
+++ Max_Min/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int d = Convert.ToInt32(Console.ReadLine());
+
+if (a < d)
+{
+    Console.WriteLine($"Максимальное число {d}, Минимальное {a}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {a}, Минимальное {d}");
+}
